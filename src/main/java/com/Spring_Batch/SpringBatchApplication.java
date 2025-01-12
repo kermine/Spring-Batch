7731965135b2c8fe7390fde502f7535594d672a4
Hernando Aragón <vhernando@minsait.com>
2025-01-12 12:03:32 +0100

    new commit

---
+++ src/main/java/com/Spring_Batch/SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
